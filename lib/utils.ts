e6d95df4c1bf852f6caee313a603341158251df2
wahyunf354 <wahyunurfadillah313@gmail.com>
2025-12-23 15:10:20 +0700

    fix gagal build

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,20 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function formatRupiah(value: number) {
+  if (value === null || value === undefined) return "Rp 0";
+
+  const number = Number(String(value).replace(/[^0-9]/g, ""));
+
+  if (isNaN(number)) return "Rp 0";
+
+  return new Intl.NumberFormat("id-ID", {
+    style: "currency",
+    currency: "IDR",
+    minimumFractionDigits: 0,
+  }).format(number);
 }
